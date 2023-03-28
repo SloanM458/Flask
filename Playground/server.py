0301df690fb5fadbebc89e4aca66b436998479f6
Sloan Molstad <sloan.molstad@gmail.com>
2023-03-27 18:11:50 -0700

    updating work

--- Playground/server.py
+++ Playground/server.py
@@ -1,8 +1,5 @@
 # TO WHOEVER IS GRADING- JUSTIN HELPED ME WITH SOME OF THIS, THERE MAY BE SIMILARITIES
 
-# I AM UNSURE WHY BUT MY CODE DOESN'T SEEM TO BE WORKING. I HAVE UNINSTALLED THE PIPENV AND REINSTALLED IT AND IT STILL GIVES ME ERRORS. 
-#I WILL BE SPEAKING WITH A TA TOMORROW ON ZOOM TO GET REAL TIME FEEDBACK. THANK YOU! :)
-
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -14,7 +11,7 @@ app = Flask(__name__)
 
 @app.route('/play')
 def level_one():
-    return render_template("index.html", num=3, color = "aqua")
+    return render_template("index.html", x= 3, color = "aqua")
 
 
 @app.route ('/level_two/<int:x>/<string:color>')
